db02ddebd877854424c0c2d1314d9c0e21aed740
ssadrian <113125604+ssadrian@users.noreply.github.com>
2023-01-16 20:13:30 +0100

    Fix routing

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,9 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {LoginPageComponent} from "./login-page/login-page.component";
 import {MainPageComponent} from "./main-page/main-page.component";
 
 export const routes: Routes = [
-  {path: "login", component: LoginPageComponent},
-  {path: "main", component: MainPageComponent},
+  {path: "", component: MainPageComponent},
 ];
 
 @NgModule({
